216e9eadb21ef2aad429ac78a07967e6e7a1a4fa
kodenart <mjuanvitorp@gmail.com>
2022-02-21 15:41:51 -0300

    lint fix on req 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { getByRole, getByTestId, getByText, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { screen, getByRole } from '@testing-library/react';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
