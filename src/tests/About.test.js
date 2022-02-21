2496603d3a902628052f48d8e30831bc8a5a672f
kodenart <mjuanvitorp@gmail.com>
2022-02-21 13:10:14 -0300

    req 1 e 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,36 @@
-test('', () => {});
+import React from 'react';
+import { getByRole, screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { About } from '../components';
+
+describe('testing the About component...', () => {
+  it('tests if theres info about what is a pokedex', () => {
+    renderWithRouter(<About />);
+    const infoText = /One can filter Pokémons by type, and see more details/;
+    const info = screen.getByText(infoText);
+    expect(info).toBeInTheDocument();
+  });
+
+  it('tests if theres a h2 with About Pokédex as text', () => {
+    const { container } = renderWithRouter(<About />);
+    const titleText = /About Pokédex/;
+    // did without screen just to learn how to do it actually... turns out I did not knew before the attempt
+    const title = getByRole(container, 'heading', { level: 2, name: titleText });
+    expect(title).toBeInTheDocument();
+  });
+
+  it('tests if theres two paragraphs', () => {
+    renderWithRouter(<About />);
+    const firstP = screen.getByText(/This application simulates a Pokédex.*all Pokémons/);
+    expect(firstP).toBeInTheDocument();
+    const secP = screen.getByText(/One can filter Pokémons.*see more details.*of them/);
+    expect(secP).toBeInTheDocument();
+  });
+
+  it('tests if theres a specific img', () => {
+    renderWithRouter(<About />);
+    const theUrl = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const theImg = screen.getByRole('img');
+    expect(theImg.src).toBe(theUrl);
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,64 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testing the App component...', () => {
+  it('tests if there`s three fixed nav links', () => {
+    renderWithRouter(<App />);
+    // Home LINK
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+    expect(homeLink).toBeInTheDocument();
+    // About LINK
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    expect(aboutLink).toBeInTheDocument();
+    // Favorite LINK
+    const favLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(favLink).toBeInTheDocument();
+  });
+
+  it('checks if the pathname is correct for homeLink', () => {
+    const { history } = renderWithRouter(<App />);
+
+    // grabs the link
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+    expect(homeLink).toBeInTheDocument();
+    userEvent.click(homeLink);
+    // checks if we're at Home pathname
+    const homePath = history.location.pathname;
+    expect(homePath).toBe('/');
+  });
+
+  it('checks if the pathname is correct for aboutLink', () => {
+    const { history } = renderWithRouter(<App />);
+
+    // grabs the link
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    expect(aboutLink).toBeInTheDocument();
+    userEvent.click(aboutLink);
+    // checks if we're at about pathname
+    const aboutPath = history.location.pathname;
+    expect(aboutPath).toBe('/about');
+  });
+
+  it('checks if the pathname is correct for favLink', () => {
+    const { history } = renderWithRouter(<App />);
+
+    // grabs the link
+    const favLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(favLink).toBeInTheDocument();
+    userEvent.click(favLink);
+    // checks if we're at favorite pathname
+    const favPath = history.location.pathname;
+    expect(favPath).toBe('/favorites');
+  });
+
+  it('checks if the router is redirecting to the Not Found page', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/random-url-to-test-the-router');
+    // checks if we're at favorite pathname
+    const notFoundHeading = screen.getByText(/Page requested not found/i);
+    expect(notFoundHeading).toBeInTheDocument();
+  });
+});
